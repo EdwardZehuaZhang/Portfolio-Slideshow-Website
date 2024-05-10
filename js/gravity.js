55dfa6d6f790b80e1c04ac0b22a7e76671aeb155
Edward Zhang <99872318+Yangtze-River-Full-of-Milkteaaa@users.noreply.github.com>
2024-05-10 21:56:03 +0700

    logo drop animation, full clean up

---
+++ js/gravity.js
@@ -0,0 +1,69 @@
+const Engine = Matter.Engine,
+      Render = Matter.Render,
+      World = Matter.World,
+      Bodies = Matter.Bodies;
+
+const engine = Engine.create();
+const world = engine.world;
+
+engine.gravity.y = 1.0; 
+
+const render = Render.create({
+    element: document.getElementById("matter-container"),
+    engine: engine,
+    options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        background: 'transparent',
+        wireframes: false
+    }
+});
+
+Engine.run(engine);
+Render.run(render);
+
+const ground = Bodies.rectangle(
+    window.innerWidth ,
+    window.innerHeight ,
+
+    {
+        isStatic: true,
+        friction: 1.2, 
+        restitution: 0.1, 
+        render: {
+            visible: false
+        }
+    }
+);
+World.add(world, ground);
+
+function addFallingImage() {
+    const imageWidth = 2; 
+    const imageHeight = 2;
+
+    const imageBody = Bodies.rectangle(
+        Math.random() * window.innerWidth,
+        -50,
+        imageWidth,
+        imageHeight,
+        {
+            angle: Math.random() * Math.PI * 2,
+            restitution: 0.2,
+            friction: 1.2,
+            frictionAir: 0.15,
+            density: 0.001,
+            angularDamping: 0.99, 
+            render: {
+                sprite: {
+                    texture: "assets/长江奶茶.png",
+                    xScale: imageWidth / 100,
+                    yScale: imageHeight / 100
+                }
+            }
+        }
+    );
+
+    Matter.Body.setAngularVelocity(imageBody, 0.03);
+
+    World.add(world, imageBody);
+}
---
+++ js/main.js
@@ -0,0 +1,41 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+let isTransitioning = false;
+
+function showSlide(index) {
+    currentSlide = (index + slides.length) % slides.length;
+
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active-slide", i === currentSlide);
+    });
+
+    isTransitioning = false;
+
+    addFallingImage();
+}
+
+function changeSlide(offset) {
+    if (!isTransitioning) {
+        isTransitioning = true;
+        showSlide(currentSlide + offset);
+    }
+}
+
+prevButton.onclick = () => changeSlide(-1);
+nextButton.onclick = () => changeSlide(1);
+
+document.addEventListener("keydown", (e) => {
+    if (!isTransitioning) {
+        if (e.key === "ArrowLeft") {
+            changeSlide(-1);
+        } else if (e.key === "ArrowRight") {
+            changeSlide(1);
+        }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    showSlide(currentSlide);
+});
