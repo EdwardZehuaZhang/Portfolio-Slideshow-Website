52a2e321d2dddd00df00273157018a884f13bea0
Edward Zhang <99872318+Yangtze-River-Full-of-Milkteaaa@users.noreply.github.com>
2024-05-10 23:17:31 +0700

    Retry

--- js/gravity.js
+++ js/gravity.js
@@ -56,8 +56,8 @@ function addFallingImage() {
             render: {
                 sprite: {
                     texture: "assets/长江奶茶.png",
-                    xScale: imageWidth / 300,
-                    yScale: imageHeight / 300
+                    xScale: imageWidth / 100,
+                    yScale: imageHeight / 100
                 }
             }
         }
