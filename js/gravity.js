1399c825f92f820c6bd54db3606cb7fe67d05165
Yangtze River Full of Milkteaaa <99872318+Yangtze-River-Full-of-Milkteaaa@users.noreply.github.com>
2024-05-10 23:57:39 +0700

    Update gravity.js

--- js/gravity.js
+++ js/gravity.js
@@ -56,8 +56,8 @@ function addFallingImage() {
             render: {
                 sprite: {
                     texture: "assets/长江奶茶.png",
-                    xScale: imageWidth / 260,
-                    yScale: imageHeight / 260
+                    xScale: imageWidth / 250,
+                    yScale: imageHeight / 250
                 }
             }
         }
